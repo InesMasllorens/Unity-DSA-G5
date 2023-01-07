7fab60d7baa33c423dc09532e4f7c56731fa8933
CarlaVictorio <carla.victorio@estudiantat.upc.edu>
2023-01-07 23:27:12 +0100

    player i enemies moving

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,15 @@ using Completed;
 
     public class GameManager : MonoBehaviour
     {
-
+        public float turnDelay = .1f;
         public static GameManager instance = null;                //Static instance of GameManager which allows it to be accessed by any other script.
         private BoardManager boardScript;                        //Store a reference to our BoardManager which will set up the level.
+        public int playerFoodPoints = 100;
+        [HideInInspector] public bool playersTurn = true;
+        
         private int level = 3;                                    //Current level number, expressed in game as "Day 1".
+        private List<Enemy> enemies;
+        private bool enemiesMoving;
 
         //Awake is always called before any Start functions
         void Awake()
@@ -28,7 +33,7 @@ using Completed;
 
             //Sets this to not be destroyed when reloading scene
             DontDestroyOnLoad(gameObject);
-
+            enemies = new List<Enemy>();
             //Get a component reference to the attached BoardManager script
             boardScript = GetComponent<BoardManager>();
 
@@ -39,16 +44,66 @@ using Completed;
         //Initializes the game for each level.
         void InitGame()
         {
+            enemies.Clear();
             //Call the SetupScene function of the BoardManager script, pass it current level number.
             boardScript.SetupScene(level);
 
         }
-
+        public void GameOver()
+        {
+            enabled = false;
+        }
 
 
         //Update is called every frame.
         void Update()
         {
+        //Check that playersTurn or enemiesMoving or doingSetup are not currently true.
+        if (playersTurn || enemiesMoving)
 
-        }
+            //If any of these are true, return and do not start MoveEnemies.
+            return;
+
+        //Start moving enemies.
+        StartCoroutine(MoveEnemies());
+
     }
+    //Call this to add the passed in Enemy to the List of Enemy objects.
+    public void AddEnemyToList(Enemy script)
+    {
+        //Add Enemy to List enemies.
+        enemies.Add(script);
+    }
+
+    //Coroutine to move enemies in sequence.
+    IEnumerator MoveEnemies()
+    {
+        //While enemiesMoving is true player is unable to move.
+        enemiesMoving = true;
+
+        //Wait for turnDelay seconds, defaults to .1 (100 ms).
+        yield return new WaitForSeconds(turnDelay);
+
+        //If there are no enemies spawned (IE in first level):
+        if (enemies.Count == 0)
+        {
+            //Wait for turnDelay seconds between moves, replaces delay caused by enemies moving when there are none.
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        //Loop through List of Enemy objects.
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            //Call the MoveEnemy function of Enemy at index i in the enemies List.
+            enemies[i].MoveEnemy();
+
+            //Wait for Enemy's moveTime before moving next Enemy, 
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        //Once Enemies are done moving, set playersTurn to true so player can move.
+        playersTurn = true;
+
+        //Enemies are done moving, set enemiesMoving to false.
+        enemiesMoving = false;
+    }
+}
