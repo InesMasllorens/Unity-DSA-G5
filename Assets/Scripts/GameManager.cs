5907c4cc664c4f4bc162dc3e5131d4f9678d9e33
Ines Masllorens <ines.maria.masllorens@estudiantat.upc.edu>
2023-01-08 15:21:37 +0100

    ingredientes y utensilios y video 12

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
 using System.Collections;
 using Completed;
+using UnityEngine.UI;
 
     using System.Collections.Generic;        //Allows us to use Lists. 
 
     public class GameManager : MonoBehaviour
     {
-        public float turnDelay = .1f;
+        public float levelStartDelay = 2f;                        //Time to wait before starting level, in seconds.
+        public float turnDelay = .1f;                             //Delay between each Player turn.
         public static GameManager instance = null;                //Static instance of GameManager which allows it to be accessed by any other script.
         private BoardManager boardScript;                        //Store a reference to our BoardManager which will set up the level.
         public int playerFoodPoints = 100;
         [HideInInspector] public bool playersTurn = true;
-        
-        private int level = 3;                                    //Current level number, expressed in game as "Day 1".
+
+
+
+        private Text levelText;                                    //Text to display current level number.
+        private GameObject levelImage;                            //Image to block out level as levels are being set up, background for levelText.
+        private int level = 1;                                    //Current level number, expressed in game as "Day 1".
         private List<Enemy> enemies;
         private bool enemiesMoving;
+        private bool doingSetup = true;                            //Boolean to check if we're setting up board, prevent Player from moving during setup.
 
-        //Awake is always called before any Start functions
-        void Awake()
+    //Awake is always called before any Start functions
+    void Awake()
         {
             //Check if instance already exists
             if (instance == null)
@@ -41,33 +48,78 @@ using Completed;
             InitGame();
         }
 
-        //Initializes the game for each level.
-        void InitGame()
+    //This is called each time a scene is loaded.
+    void OnLevelWasLoaded(int index)
+    {
+        //Add one to our level number.
+        level++;
+        //Call InitGame to initialize our level.
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
         {
+            //While doingSetup is true the player can't move, prevent player from moving while title card is up.
+            doingSetup = true;
+
+            //Get a reference to our image LevelImage by finding it by name.
+            levelImage = GameObject.Find("LevelImage");
+
+            //Get a reference to our text LevelText's text component by finding it by name and calling GetComponent.
+            levelText = GameObject.Find("LevelText").GetComponent<Text>();
+
+            //Set the text of levelText to the string "Day" and append the current level number.
+            levelText.text = "Day " + level;
+
+            //Set levelImage to active blocking player's view of the game board during setup.
+            levelImage.SetActive(true);
+
+            //Call the HideLevelImage function with a delay in seconds of levelStartDelay.
+            Invoke("HideLevelImage", levelStartDelay);
+
             enemies.Clear();
             //Call the SetupScene function of the BoardManager script, pass it current level number.
             boardScript.SetupScene(level);
 
         }
-        public void GameOver()
-        {
-            enabled = false;
-        }
+
+    //Hides black image used between levels
+    void HideLevelImage()
+    {
+        //Disable the levelImage gameObject.
+        levelImage.SetActive(false);
+
+        //Set doingSetup to false allowing player to move again.
+        doingSetup = false;
+    }
 
 
-        //Update is called every frame.
-        void Update()
-        {
+    public void GameOver()
+    {
+        //Set levelText to display number of levels passed and game over message
+        levelText.text = "In level " + level + " , you haven't complete the recipe.";
+
+        //Enable black background image gameObject.
+        levelImage.SetActive(true);
+
+        enabled = false;
+    }
+
+
+    //Update is called every frame.
+    void Update()
+    {
         //Check that playersTurn or enemiesMoving or doingSetup are not currently true.
-        if (playersTurn || enemiesMoving)
+        if (playersTurn || enemiesMoving || doingSetup)
 
             //If any of these are true, return and do not start MoveEnemies.
             return;
 
         //Start moving enemies.
         StartCoroutine(MoveEnemies());
-
     }
+
     //Call this to add the passed in Enemy to the List of Enemy objects.
     public void AddEnemyToList(Enemy script)
     {
